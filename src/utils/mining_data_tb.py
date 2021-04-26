ba774912b7cf3c7500ffce7ef35c75ab3f7ff047
Mariacagi <mariacagigas.s@gmail.com>
2021-04-26 18:28:27 +0200

    Finally dataframe of sellout and sellin

--- src/utils/mining_data_tb.py
+++ src/utils/mining_data_tb.py
@@ -3,21 +3,17 @@ from varname import nameof
 from varname.helpers import Wrapper
 
 def changing_dates(df, name_column): 
-    """changing the date type to datatime"""
+    """Changing the date type to datatime"""
     df[name_column] = df[name_column].astype('datetime64[ns]')
 
 
 def split_column(df, column, separation_object, times):
+    """Split a column of a dataframe"""
     df[column] = df[column].str.split(separation_object, n=times, expand=True)
 
 
 def check_dupli_nan(df_name, df):
+    """Checkinf if there is any duplicate or any null"""
     print(df_name, "duplicates", "---->", df.duplicated().values.any())
     print(df_name, "null",  "---->", df.isnull().values.any())
-    # if df.duplicated().values.any() == True:
-    #     df1 = df.drop_duplicates()
-    #     print("\nDeleting the duplicates of", df_name, "\n    Duplicates now is", "---->", df1.duplicated().values.any())
-    #     df=df1
     print("-------------------------- \n")
-
-    # return df
