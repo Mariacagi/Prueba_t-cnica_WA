39a9864566f3b1e92ba8849de31ed0a09f5a9a31
Mariacagi <mariacagigas.s@gmail.com>
2021-04-28 16:34:22 +0200

    Added first final csv, reports and main

--- src/utils/visualization_tb.py
+++ src/utils/visualization_tb.py
@@ -7,17 +7,20 @@ def cm_to_inch(value):
     return value/2.54
 
 
-def histogram_figure(x, y, x_label, y_label, rooth_path, name_fig):
+def histogram_figure(x, y, x_label, y_label, rooth_path, name_fig, ylim1=None, ylim2=None):
     """Creating an histogram """
 
     # Creating the figure
-    plt.figure(figsize=(cm_to_inch(20),cm_to_inch(10)))
-    plt.bar(x=x, height=y, facecolor='#222220')
+    sns.set()
+    plt.figure(figsize=(cm_to_inch(25),cm_to_inch(15)))
+    plt.bar(x=x, height=y, facecolor='#3d4248')
 
     # Labels
     plt.xlabel(x_label, weight="bold")
     plt.ylabel(y_label, weight="bold")
     plt.xticks(rotation="90")
+    if ylim1 != None:
+        plt.ylim(ylim1, ylim2)
 
     # Saving the figure
     carpeta_reports = "\\reports"
@@ -31,8 +34,9 @@ def linediagram_figure(x, y, x_label, y_label, rooth_path, name_fig):
     """ Creating a line diagram """
 
     # Creating the figure
-    plt.figure(figsize=(cm_to_inch(20),cm_to_inch(10)))
-    plt.plot(x, y, color='#434343', marker=".", markersize=15, linewidth= 3, label="Year")
+    sns.set()
+    plt.figure(figsize=(cm_to_inch(25),cm_to_inch(15)))
+    plt.plot(x, y, color='#3d4248', marker=".", markersize=15, linewidth= 3, label="Year")
 
     # Labels
     plt.xlabel(x_label, weight="bold")
@@ -61,12 +65,31 @@ def correlation_matrix(df):
     plt.yticks(va="center", rotation = 0)
 
 
-def piechart_figure_6labels(x1, x2, x3, x4, x5, x6, labels, name_fig, rooth_path):
+def piechart_figure_6labels(labels, name_fig, rooth_path, x1=None, x2=None, x3=None, x4=None, x5=None, x6=None):
     
     # Creating the figure
+    sns.set()
     plt.figure(figsize=(cm_to_inch(20),cm_to_inch(20)))
-    colors = ["#222220","#2C2C2C", "#434343", "#62635D", "#868781", "#D6D6D4"]
-    plt.pie([x1, x2, x3, x4, x5, x6], labels = labels, colors=colors, autopct="%.1f %%")    
+
+    if x6 != None:
+        colors = ["#3d4248","#3f5673", "#4D6380", "#5579A9", "#6B737E", "#C4CBD6"]
+        plt.pie([x1, x2, x3, x4, x5, x6], labels = labels, colors=colors, autopct="%.1f %%")
+
+    elif x5 != None:
+        colors = ["#3f5673", "#4D6380", "#5579A9", "#6B737E","#C4CBD6"]
+        plt.pie([x1, x2, x3, x4, x5], labels = labels, colors=colors, autopct="%.1f %%")
+
+    elif x4 != None:
+        colors = ["#4D6380", "#5579A9", "#6B737E","#C4CBD6"]
+        plt.pie([x1, x2, x3, x4], labels = labels, colors=colors, autopct="%.1f %%")
+    
+    elif x3 != None:
+        colors = ["#5579A9", "#6B737E","#C4CBD6"]
+        plt.pie([x1, x2, x3], labels = labels, colors=colors, autopct="%.1f %%")
+
+    elif x2 != None:
+        colors = ["#5579A9","#C4CBD6"]
+        plt.pie([x1, x2], labels = labels, colors=colors, autopct="%.1f %%")    
 
     # Saving the figure
     carpeta_reports = "\\reports"
